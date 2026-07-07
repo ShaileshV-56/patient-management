cf0f1ee6f507b7eb3817275c52e5b16749d6e5ea
ShaileshV-56 <shaileshveerapathiran333@gmail.com>
2026-07-07 20:26:04 +0530

    feat(api-gateway): configure service routing

--- api-gateway/src/main/java/com/pm/apigateway/config/RateLimiterConfig.java
+++ api-gateway/src/main/java/com/pm/apigateway/config/RateLimiterConfig.java
@@ -1,4 +1,18 @@
 package com.pm.apigateway.config;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
 public class RateLimiterConfig {
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(
+                exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+    }
+
+    // IP = 123.121.3.6 COUNT = 10
 }
